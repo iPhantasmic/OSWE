7f2eacb6492063518fdda94a882413463efaff1e
Justin <justin.100600@gmail.com>
2023-07-22 03:13:38 +0800

    new function for HTTP POST form

--- scripts/utils/requests.go
+++ scripts/utils/requests.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -25,7 +26,69 @@ func SendGetRequest(client *http.Client, debug bool, requestURL string) Response
 	}
 
 	if debug {
-		PrintInfo("Sending HTTP request to: " + requestURL)
+		PrintInfo("Sending HTTP GET request to: " + requestURL)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("[-] Failed to send HTTP request: ", err)
+	}
+	defer resp.Body.Close()
+	if debug {
+		PrintSuccess("Got HTTP response!")
+	}
+
+	// get HTTP response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln("[-] Failed to read HTTP response body: ", err)
+	}
+	bodyString := string(body)
+
+	// get HTTP response headers
+	respHeaders := make(map[string]string)
+	for headerKey, headerValues := range resp.Header {
+		respHeaders[headerKey] = strings.Join(headerValues, ", ")
+	}
+
+	if debug {
+		// print HTTP status code
+		PrintInfo(fmt.Sprintf("HTTP response status code: %d", resp.StatusCode))
+
+		// print HTTP content length
+		PrintInfo(fmt.Sprintf("HTTP response content length: %d", resp.ContentLength))
+
+		// print HTTP response body
+		PrintInfo("Response body: ")
+		fmt.Println(bodyString)
+
+		// print HTTP response headers
+		PrintInfo("Response headers: ")
+		for header, value := range respHeaders {
+			fmt.Printf("\t%s = %s\n", header, value)
+		}
+
+		fmt.Println("")
+	}
+
+	return Response{
+		StatusCode:      resp.StatusCode,
+		ContentLength:   resp.ContentLength,
+		ResponseBody:    bodyString,
+		ResponseHeaders: respHeaders,
+	}
+}
+
+func SendPostRequestForm(client *http.Client, debug bool, requestURL string, data url.Values) Response {
+	// create our HTTP POST request
+	req, err := http.NewRequest(http.MethodPost, requestURL, strings.NewReader(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	// we are not using client.PostForm() so we have to specify the Content-Type
+	if err != nil {
+		log.Fatalln("[-] Failed to create HTTP request: ", err)
+	}
+
+	if debug {
+		PrintInfo("Sending HTTP POST request to: " + requestURL)
 	}
 	resp, err := client.Do(req)
 	if err != nil {
