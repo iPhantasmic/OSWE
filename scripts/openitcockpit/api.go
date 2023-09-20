bfa021efb7f003e9486ca92e98b4887d644e0d31
Justin <justin.100600@gmail.com>
2023-09-20 16:57:01 +0800

    ex 10.6.4.1 - POST to upload content

--- scripts/openitcockpit/api.go
+++ scripts/openitcockpit/api.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-
+	
 	"github.com/iPhantasmic/OSWE/scripts/utils"
 )
 
+// Chp 10.6.4 - Creating the API
 func health(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, all is well!")
 }
 
 func clientJS(c *gin.Context) {
 	c.FileAttachment("./client.js", "client.js")
+}
+
+// Ex 10.6.4.1 - POST API
+func uploadContent(c *gin.Context) {
+	content := c.PostForm("content")
+	url := c.PostForm("url")
+
+	utils.PrintInfo(fmt.Sprintf("Saving to DB for URL: %s", url))
+	newId := InsertContent(url, content)
+	if newId == 0 {
+		c.String(http.StatusInternalServerError, "Failed to save new content...")
+	}
+
+	utils.PrintSuccess(fmt.Sprintf("Saved to DB for URL: %s", url))
+	c.String(http.StatusCreated, "Content for URL: %s saved to %d", url, newId)
 }
 
 func main() {
@@ -33,6 +50,7 @@ func main() {
 	// define routes and corresponding handlers
 	r.GET("/health", health)
 	r.GET("/client.js", clientJS)
+	r.POST("/content", uploadContent)
 
 	// load SSL certificate and key
 	certFile := "cert.pem"
